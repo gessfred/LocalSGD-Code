f568e149cfc1b0a2bf6ae69bd72b24a0cf1ac629
Frédéric Gessler <36667834+gessfred@users.noreply.github.com>
2020-03-18 08:26:08 +0100

    Update distributed_running_cv.py

--- distributed_code/pcode/distributed_running_cv.py
+++ distributed_code/pcode/distributed_running_cv.py
@@ -58,9 +58,9 @@ def train_and_validate(
 
             # display the logging info.
             display_training_stat(conf, scheduler, tracker_tr, n_bits_to_transmit)
-
             # finish one epoch training and to decide if we want to val our model.
             if scheduler.epoch_ % 1 == 0:
+                timer.epoch()
                 if tracker_tr.stat["loss"].avg > 1e3 or np.isnan(
                     tracker_tr.stat["loss"].avg
                 ):
