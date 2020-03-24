20ffbff0e6338c681067e0271e9df884ad8d2dba
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-24 02:41:28 +0100

    Build local from memory

--- distributed_code/pcode/optim/local_ef_sgd.py
+++ distributed_code/pcode/optim/local_ef_sgd.py
@@ -108,6 +108,7 @@ class Local_EFSGD(Optimizer):
                 ):
                     # add memory to the model difference.
                     memory.data.copy_(consensus_param - param + memory)
+                    local.append(memory)
                     # compress.
                     #_local_scale, _local_sign = scaled_sign(memory)
                 local_tb = CompressedTensorBuffer(local, self.conf.compress_width)
