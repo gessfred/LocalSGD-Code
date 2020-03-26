9bf82d18fd4829b9d40f8254f5ee7756dc9889d6
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-26 21:08:11 +0100

    Send compressed buffer alone

--- distributed_code/pcode/optim/local_ef_sgd.py
+++ distributed_code/pcode/optim/local_ef_sgd.py
@@ -134,12 +134,12 @@ class Local_EFSGD(Optimizer):
                 )
 
             # unpack the synced info and update the consensus params.
-            with kargs["timer"]("sync/update_consensus", epoch=self.conf.epoch_):
+            """with kargs["timer"]("sync/update_consensus", epoch=self.conf.epoch_):
                 for update_local, consensus_param, l1_norm, padding in zip(
                     local_tb, self.consensus_params_tb, l1_norms_tb, paddings
                 ):
                     vec = unquantize_gpu(update_local, padding, 1) * l1_norm
-                    consensus_param.view(-1).add_(-1.0, vec)
+                    consensus_param.view(-1).add_(-1.0, vec)"""
 
             # consistent the local models by assigning the consensus params.
             self.consensus_params_tb.unpack(params)
