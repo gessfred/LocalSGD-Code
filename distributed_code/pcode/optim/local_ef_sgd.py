68f9e2d66c8c8b8318a35ed4ae980df336fb86dc
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-04-11 21:32:54 +0200

    ...

--- distributed_code/pcode/optim/local_ef_sgd.py
+++ distributed_code/pcode/optim/local_ef_sgd.py
@@ -189,8 +189,6 @@ class Local_EFSGD(Optimizer):
                         local_sign, paddings, buffers
                     ):
                         recv_ed = unquantize_gpu(buffer, pad, 1)
-                        #print(sign.size(), recv_ed.size(), sign, recv_ed)
-                        print('RES', sign, pad)
                         res.append((recv_ed.view(sign.size()) + sign) / 2)
                     #res_tb = TensorBuffer(res)
                     """tmp = TensorBuffer(local_sign)
