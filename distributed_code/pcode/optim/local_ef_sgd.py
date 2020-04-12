9dd8d0c85320bf976df9312a9c9838e2d7d29351
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-04-12 18:44:25 +0200

    ...

--- distributed_code/pcode/optim/local_ef_sgd.py
+++ distributed_code/pcode/optim/local_ef_sgd.py
@@ -168,7 +168,7 @@ class Local_EFSGD(Optimizer):
                         compressed.append(d)
                         paddings.append(p)
                         copies.append(memory.view(-1).clone())
-                        print(d[:3], 'vs.', _local_sign[:3], 'from', memory.data[:3])
+                        print(d[:3], 'vs.', _local_sign[:3], 'from', memory.data.view(-1)[:3])
                         torch.cuda.synchronize()
                         # store local scales and local sign.
                         local_scale.append(_local_scale)
