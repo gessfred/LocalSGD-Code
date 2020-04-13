a6d2ec5020f624f7f08d01b74343ca56cf263f82
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-04-13 15:03:55 +0200

    ...

--- distributed_code/pcode/optim/local_ef_sgd.py
+++ distributed_code/pcode/optim/local_ef_sgd.py
@@ -187,18 +187,18 @@ class Local_EFSGD(Optimizer):
                     #simple exchange
                     way1 = directions_tb.buffer if self.rank == 0 else buffers.buffer
                     way2 = buffers.buffer if self.rank == 0 else directions_tb.buffer
-                    if rank == 0:
-                        print('[{}]sending'.format(self.rank), way1[:30])
+                    if self.rank == 0:
+                        print('[{}]sending'.format(self.rank), way1.type(), way1[:30])
                     else:
-                        print('[{}]sending'.format(self.rank), way2[:30])
-                    dist.broadcast(way1, 0)
-                    dist.broadcast(way2, 1)
+                        print('[{}]sending'.format(self.rank), way2.type(), way2[:30])
+                    dist.broadcast(way1.type(torch.FloatTensor), 0)
+                    dist.broadcast(way2.type(torch.FloatTensor), 1)
                     res = []
                     sub = []
                     for  sign, pad, buffer in zip(
                         local_sign, paddings, buffers
                     ):
-                        recv_ed = unquantize_gpu(buffer, pad, 1)
+                        recv_ed = unquantize_gpu(buffer.type(torch.IntTensor), pad, 1)
                         sub.append(recv_ed.view(sign.size()))
                         res.append((recv_ed.view(sign.size()) + sign) / 2)
                     #res_tb = TensorBuffer(res)
